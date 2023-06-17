b288d3e2194ba0757f4c4dfb52521ef96b5235a3
CodeFromEggly <wellshg10@gmail.com>
2023-06-17 12:47:41 +1200

    Homepage has form to enter source wallet and Eterscan API. D3 force simulation graph displays after POST to /submit route. visualisation.css is not currently accessed.

--- public/visualisation/visualisation.js
+++ public/visualisation/visualisation.js
@@ -1,120 +1,66 @@
-console.log("Loading http request");
-import { APIKEY } from '../api.js';
-// TODO import api from .env which means loading api calls server-side
+console.log("Visualising data");
 
-// Source wallet:
-const SOURCE = "0xDa06d7bF334bc5cCec9CB76de194565937378140";
+/*
+Unpack data
+*/
 
-// Create query address using SOURCE and apikey
-const API_ADDRESS = `https://api.etherscan.io/api?module=account&action=txlist&address=${SOURCE}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${APIKEY}`;
+const SOURCE = data.sourceWallet;
+var nodes = data.nodes;
+var links = data.links;
+var wallets = data.wallets;
 
+console.log(nodes);
+console.log(links);
 
-(async () => {
-  try {
-    const response = await fetch(API_ADDRESS);
-    var data = await response.json();
-    console.log(data);
-    var numTrans = data.result.length;
-  
-    // Prepare data for D3:
-    var wallets = data.result.reduce(function (acc, val) {
-      if (acc.indexOf(val.to) === -1) {
-        acc.push(val.to);
-      }
-      return acc;
-    }, []);
-  
-    // When creating Link D3 appears to need the source wallet also
-    // Insert source wallet at start
-    wallets.unshift(SOURCE)
-  
-  
-    // Generate nodes for the graph from each wallet
-    var nodes = [];
-    wallets.forEach(function (wallet) {
-      var nodeObject = {
-        name: wallet
-      }
-      nodes.push(nodeObject);
-    });
-
-    // create links for d3 which required numerical IDs not names
-    var links = data.result.map(function (val) {
-      return {
-        source: nodes[0], // source node is the same for all links
-        target: nodes[wallets.indexOf(val.to)] // target node is the node that has the same index as val.to in the wallets array
-      };
-    });
-  
-    console.log(`Wallets: ${wallets} and Nodes: ${nodes}`);
-    console.log(`Links: ${links}`);
-    // TODO convert to arrow notation to be 5exy
-    /*
-    wallets.array.forEach(element => {
-    
-    });
-    */
-  
-    // Visualise the results using D3.js:
-    createVisualisation(nodes, links);
-  } catch (error) {
-    console.error(error);
-  }
-})();
 
 
 /*
 Visualising the results
 */
 
-function createVisualisation(nodes, links) {
+// Display SVG in 3/4 of the window size
+const height = window.innerHeight * 0.75;
+const width = window.innerWidth * 0.75;
+const nodeRadius = 5;
 
-  console.log("createVisualisation");
-  console.log(d3.version);
-  
-  const height = 800;
-  const width = 800;
-  const nodeRadius = 5;
+// force-directed graph layout
+const force = d3.forceSimulation(nodes) // nodes repel each other
+  .force("charge", d3.forceManyBody().strength(-220)) 
+  .force("link", d3.forceLink(links).distance(100))
+  .force("center", d3.forceCenter(width / 2, height / 2));
 
-  // force-directed graph layout
-  const force = d3.forceSimulation(nodes) // nodes repel each other
-    .force("charge", d3.forceManyBody().strength(-220)) 
-    .force("link", d3.forceLink(links).distance(100))
-    .force("center", d3.forceCenter(width / 2, height / 2));
+// Give the force layout a chance to move the nodes a bit
+for (let i = 0; i < 300; ++i) force.tick();
 
-  // Give the force layout a chance to move the nodes a bit
-  for (let i = 0; i < 300; ++i) force.tick();
+// Here is where D3 begins to manipulate the DOM
+const svg = d3.select("#root").append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  // Log nodes to see their current x and y properties
-  console.log(nodes);
-  console.log(links);
 
-  // Here is where D3 begins to manipulate the DOM
-  const svg = d3.select("#root").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const link =  svg.selectAll(".link")
+  .data(links)
+  .join("line")
+  //.attr("class", "link")
+  .attr("stroke", "#f80a0a");
 
-  const link =  svg.selectAll(".link")
-    .data(links)
-    .join("line")
-    .attr("class", "link");
+const node = svg.selectAll(".node")
+  .data(nodes)
+  .join("circle")
+  //.attr("class", "node")
+  .attr("stroke","#f80a0a")
+  .attr("r", nodeRadius);
 
-  const node = svg.selectAll(".node")
-    .data(nodes)
-    .join("circle")
-    .attr("class", "node")
-    .attr("r", nodeRadius);
-
-  force.on("tick", function () {
-    link
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
+force.on("tick", function () {
+  link
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
 
     node
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-  });
+    // Create a bounding box to contain the nodes within a nodeRadius margin of the SVG
+    .attr("cx", function(d) { return d.x = Math.max(nodeRadius, Math.min(width - nodeRadius, d.x)); })
+    .attr("cy", function(d) { return d.y = Math.max(nodeRadius, Math.min(height - nodeRadius, d.y)); });
 
-}
+});
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const path = require('path');
+
+const express = require('express');
 const app = express();
 
 // Set the view engine to pug
@@ -11,12 +14,110 @@ app.set('views', path.join(__dirname, '../views'));
 // Set the static files directory
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Set up body-parser middleware to handle HTML form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Define a route to render the homepage
 app.get('/', (req, res) => {
-    res.render('visualisation', { title: 'Homepage' });
+    res.render('homepage', { title: 'Homepage' });
 });
+
+
+/*
+Routes
+*/
+
+// Route to handle form submission
+app.post('/submit', async (req, res) => {
+    const params = req.body;
+    const processedData = await getAndProcessData(params);
+
+    res.render('visualisation', {title: 'Visualisation', data: processedData});
+  });
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
+
+/* 
+Functions
+*/
+
+
+const getEtherscanData = async (params) => {
+  console.log("Loading http request via getEtherscanData");
+
+  var API_ADDRESS = "https://api.etherscan.io/api?module=account&action=txlist&address=" +
+  params.sourceWallet +
+  "&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=" +
+  params.etherscanApi;
+  // TODO add other params, i.e., amount of transactions returned
+
+  try {
+      const response = await fetch(API_ADDRESS);
+      var data = await response.json();
+      //console.log("Response data", data);
+
+  } catch (error) {
+    console.error(error);
+    // TODO Handle errors as they be passed on to processData() as undefined
+  }
+
+  // Add the original wallet address to data for later
+  data.sourceWallet = params.sourceWallet;
+  return data;
+};
+
+
+const processData = (data) => {
+  // Process data into Nodes and Links
+
+  // Accumulate list of addresses that transacted
+  var wallets = data.result.reduce(function (acc, val) {
+      if (acc.indexOf(val.to) === -1) {
+        acc.push(val.to);
+      }
+      if (acc.indexOf(val.from) === -1) {
+        acc.push(val.from);
+      }
+      return acc;
+      }, []);
+  
+  
+  // Generate nodes for the graph from each wallet
+  // TODO arrow notation using 'for each'
+
+  var nodes = [];
+  wallets.forEach(wallet => {
+      var nodeObject = {
+        id: wallet
+      }
+      nodes.push(nodeObject);
+  });
+
+  // create links for d3 which required numerical IDs not names
+  var links = data.result.map(val => {
+    let link = {
+      source: wallets.indexOf(val.from),
+      target: wallets.indexOf(val.to),
+      value: val.value
+     };
+     return link;
+  });
+  
+  console.log('nodes,',nodes);
+  console.log('links,',links);
+  
+  // Send results for using in D3.js:     
+  let postProcess = {wallets, nodes, links};
+  return postProcess;
+};
+
+
+const getAndProcessData = async (params) => {
+  const data = await getEtherscanData(params);
+  return processData(data);
+};
